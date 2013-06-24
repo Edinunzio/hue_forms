90ce9fbbfdf9104ed8d3bda55225d6bfe95f8f0b
Edinunzio <e.dinunzio@gmail.com>
2013-06-24 15:00:01 -0400

    updating template structure, html for dashboard

--- hue_forms/settings.py
+++ hue_forms/settings.py
@@ -112,7 +112,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/elizabeth/workspace/hue_forms/template",
+    "/home/elizabeth/workspace/hue_forms/templates",
 )
 
 INSTALLED_APPS = (
